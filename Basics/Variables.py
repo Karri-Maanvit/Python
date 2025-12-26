e09e4f158cfb2555397b3a2f6a92b6e82bf3f7f1
Karri-Maanvit <maanvitkarri.personal@gmail.com>
2025-12-26 12:34:02 +0530

    Variables

---
+++ Basics/Variables.py
@@ -0,0 +1,36 @@
+# Variables in python
+
+# A variable is a container for storing data values.
+
+# There are 4 different data types in python:
+
+# 1. String  - Text data, enclosed in single or double quotes
+# Example:
+string = "Hello, World!"                 #Output: Hello, World! ; Used double quotes
+name = 'Maanvit'                        #Output: Maanvit ; Used single quotes 
+
+# 2. Integer - Whole numbers, positive or negative, without decimals
+# Example:
+age = 18                             #Output : 18 
+year = 2011                          #Output : 2011 NOTE: No quotes used 
+
+# 3. Float - same as integers but with decimal points
+# Example:
+height = 5.6                    #Output : 5.6 NOTE: No quotes used 
+weight = 59.8                    #Output : 59.8 
+
+# 4. Boolean - Represents one of two values : True or False 
+# Example:
+online = False                          #Output : False NOTE:no quotes used and capital F
+student = True                           #Output : True NOTE:no quotes used and capital T
+
+# NOTE : In Python, you do not need to declare the type of variable while creating it. 
+# The interpreter automatically detects the type based on the value assigned to it.
+# NOTE : Variable names are case-sensitive. For example, 'age' and 'Age' are considered different variables.
+# NOTE : Variable names must start with a letter (a-z, A-Z) or an underscore (_), followed by letters, digits (0-9), or underscores.
+# NOTE: Avoid using Python reserved keywords as variable names (e.g., 'if', 'for', 'while', etc.).
+
+
+# New function : print() displays the output on the screen 
+print(string)                              # Output: Hello, World! NOTE: don't use quotes here or it will print it as it is.
+
