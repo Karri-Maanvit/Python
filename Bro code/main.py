62f8f582b095f6b67d2e702fa3276500385cb302
Karri-Maanvit <maanvitkarri.personal@gmail.com>
2025-12-26 19:06:33 +0530

    Variables

--- Bro code/main.py
+++ Bro code/main.py
@@ -1,4 +1,30 @@
-# Day 1 introduction to python
+#  Variable = A container for a value (String, Integer, float, boolean)
 
-print("I like to code.")
-print("It's fun to learn.")
+#             A variable behaves as if it it the value it contains
+
+# Strings
+
+character = "naruto Uzumaki"
+drink = "coke"
+email = "maanvitkarri@gmail.com"
+
+# print() fuction
+# print(character) 
+
+# print(f"my fav character is {character}")
+
+# Integers
+
+age = 14
+quantity = 7
+
+# float 
+
+price = 7.09
+gpa = 4.9
+distance = 10.27
+
+# Boolean 
+
+is_student = True 
+under_18 = True 
